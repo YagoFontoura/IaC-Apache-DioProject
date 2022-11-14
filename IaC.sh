8a21b96c9792f3e57786927a12406ef8eaad2a25
Yago Fontoura <yagofontoura222@gmail.com>
2022-11-13 21:15:37 -0300

    Add Iac.sh

---
+++ IaC.sh
@@ -0,0 +1,16 @@
+apt-get update -y
+
+apt-get install apache2 -y
+
+apt install unzip -y
+
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+mv ./main.zip /tmp
+
+unzip /tmp/main.zip -d /var/www/html
+mv /var/www/html/linux-site-dio-main/* /var/www/html -f
+
+
+
+
